f9fcf9e55931a542d1811ffc4a60aabc67cf4bb8
koshikraj <koshik.raj@gmail.com>
2018-12-19 00:13:45 +0530

    modifies NEO interface in chapter 7

--- chapter7/NEO/proof_of_ownership_app/interface/src/App.js
+++ chapter7/NEO/proof_of_ownership_app/interface/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Neon, {rpc} from '@cityofzion/neon-js'
-// import {encode, decode} from 'base-64';
-// base58 = require("ba se-58");
+import Neon, {rpc, wallet, api, sc} from '@cityofzion/neon-js'
 import {encode, decode} from 'base-58';
+import crypto from 'crypto';
+import Dropzone from 'react-dropzone';
+import { Button, ProgressBar, ButtonToolbar } from 'react-bootstrap';
+import { Line, Circle } from 'rc-progress';
 import hexEncoding from 'crypto-js/enc-hex';
 import SHA256 from 'crypto-js/sha256';
 
@@ -58,44 +60,188 @@ String.prototype.hexDecode = function(){
     return back;
 };
 
-console.log('1234'.hexEncode());
 
 class App extends Component {
 
-    getAssetOwner() {
-        const props = {
-            scriptHash: '60a7ed582c6885addf1f9bec7e413d01abe54f1a',
-            operation: 'query',
-            args: ['f572f8ce40bf97b56bad1c6f8d62552b8b066039a9835f294ea4826629278df3'.hexEncode(),
-                ''
-            ]
-        };
 
-
-        const Script = Neon.create.script(props);
-
-        rpc.Query.invokeScript(Script).execute('http://139.59.65.33:30333').then((res) => {
-
-            this.setState({owner: scripthash_to_address(res.result.stack[0].value)});
-        });
-
-    }
 
     constructor(props) {
         super(props);
-        this.state = {owner: ''};
-        this.getAssetOwner();
+        this.state = {isConnected: false,
+            uploadStatus: 0,
+            hashValue:null,
+            fileOwner: null};
+
+
 
     }
 
-  render() {
-    return (
-      <div className="PooApp">
-          Hello: {this.state.owner}
+    registerAsset(assetID)
+    {
 
-      </div>
-    );
-  }
+        try {
+            let account = new wallet.Account('KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr');
+
+            let intents = [{
+
+                assetId: Neon.CONST.ASSET_ID.GAS,
+
+                value: new Neon.u.Fixed8(1),  // I gueesed this :)
+
+                scriptHash: '60a7ed582c6885addf1f9bec7e413d01abe54f1a'
+
+            }];
+
+            // the interesting part: what do we want to do :)
+
+            let invoke = {
+
+                scriptHash: '60a7ed582c6885addf1f9bec7e413d01abe54f1a',
+
+                operation: 'register',
+
+                args: [
+
+                    'asdsd'.hexEncode(),
+                    'AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y'.hexEncode()
+
+                ]
+
+            }
+
+            let sb = Neon.create.scriptBuilder();
+
+            sb.emitAppCall(invoke.scriptHash, invoke.operation, invoke.args, false);
+            let script = sb.str;
+            let unsignedTx = Neon.create.invocationTx();
+
+
+            let signedTx = unsignedTx.addIntent(intents).sign(account.privateKey);
+            console.log(signedTx)
+
+            // send the transaction to our net
+
+            rpc.queryRPC('http://139.59.25.30:30333', {
+
+                method: 'sendrawtransaction',
+
+                params: [signedTx],
+
+                id: 1
+
+            }); // here we could listen to the response with then/catch
+
+        }
+
+        catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    async componentDidMount(){
+
+        const mainNetNeoscan = new api.neoscan.instance("TestNet");
+        const neoscanBalance = await mainNetNeoscan.getBalance('AH4dqfuyaT1tGthQiQQ2RQ8c7Xksuphb7k');
+        console.log(neoscanBalance)
+
+    }
+
+
+    queryAsset(assetID)
+    {
+
+
+        console.log(assetID.hexEncode());
+        try {
+            const props = {
+                scriptHash: '60a7ed582c6885addf1f9bec7e413d01abe54f1a',
+                operation: 'query',
+                args: [assetID.hexEncode(),
+                    ''
+                ]
+            };
+
+
+            const Script = Neon.create.script(props);
+
+            rpc.Query.invokeScript(Script).execute('http://139.59.25.30:30333').then((res) => {
+
+                this.setState({fileOwner: scripthash_to_address(res.result.stack[0].value)});
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+
+    calculateHash(file){
+        this.setState({uploadStatus:25});
+        const reader = new FileReader();
+        reader.onload = () => {
+            const fileAsBinaryString = reader.result;
+            const hashValue = crypto.createHash('sha256').update(fileAsBinaryString).digest("hex");
+            this.setState({hashValue: hashValue,
+                fileOwner: null,
+                uploadStatus:99});
+
+        };
+        reader.onabort = () => console.log('file reading was aborted');
+        reader.onerror = () => console.log('file reading has failed');
+
+        reader.readAsBinaryString(file[0]);
+
+    }
+
+    render() {
+        return (
+            <div style={{display: "flex", alignItems: "center",
+                justifyContent: "center", flexDirection: "column",
+                "padding": 100}}>
+
+
+                <Dropzone
+                    onDrop={this.calculateHash.bind(this)}
+
+                >
+                    <p>Try dropping some files here, or click to select files to upload.</p>
+                </Dropzone>
+                {this.state.uploadStatus > 0 && this.state.uploadStatus < 100?
+                    <Line percent={this.state.uploadStatus} strokeWidth="1" style={{margin: "5%", width: "30%"}}/> : null
+
+                }
+
+                {this.state.hashValue ? <div> <b>File hash value</b>: <code>{this.state.hashValue}</code></div> : null
+
+                }
+                {this.state.fileOwner ? <div> <b>Current owner</b>: <code>{this.state.fileOwner}</code></div> : null
+
+                }
+
+
+                <ButtonToolbar  style={{margin: "5%"}}>
+                    <Button bsStyle="primary" bsSize="large" active
+                            disabled={!this.state.hashValue}
+                            onClick={(e) => this.registerAsset(this.state.hashValue)}
+                        >
+                        Register file
+                    </Button>
+                    <Button bsStyle="primary" bsSize="large" active
+                            disabled={!this.state.hashValue}
+                            onClick={(e) => this.queryAsset(this.state.hashValue)}>
+                        Check owner
+                    </Button>
+                </ButtonToolbar>
+
+
+
+
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
